f193423e1dcaf5efb2db53af3a4e82304f4a3a06
Unknown <Twipply@gmail.com>
2018-06-24 14:48:10 +0100

    Separate piece mobility

---
+++ src/mobility.cpp
@@ -0,0 +1,45 @@
+#include "mobility.hpp"
+#include "bitboards.hpp"
+#include "other.hpp"
+
+int piece_mobility(const Position &pos)
+{
+    uint64_t moves = 0;
+    uint64_t copy = 0;
+    uint64_t allowed = ~pos.colour[Colour::US];
+    int from = 0;
+
+    // Knights
+    copy = pos.pieces[PieceType::KNIGHT] & pos.colour[Colour::US];
+    while(copy)
+    {
+        from = lsbll(copy);
+        moves |= magic_moves_knight(from) & allowed;
+
+        copy &= copy-1;
+    }
+
+    // Bishops & Queens
+    copy = (pos.pieces[PieceType::BISHOP] | pos.pieces[PieceType::QUEEN]) & pos.colour[Colour::US];
+    while(copy)
+    {
+        from = lsbll(copy);
+        moves |= magic_moves_bishop((pos.colour[Colour::US]|pos.colour[Colour::THEM]), from) & allowed;
+
+        copy &= copy-1;
+    }
+
+    // Rooks & Queens
+    copy = (pos.pieces[PieceType::ROOK] | pos.pieces[PieceType::QUEEN]) & pos.colour[Colour::US];
+    while(copy)
+    {
+        from = lsbll(copy);
+        moves |= magic_moves_rook((pos.colour[Colour::US]|pos.colour[Colour::THEM]), from) & allowed;
+
+        copy &= copy-1;
+    }
+
+    int count = popcountll(moves);
+
+    return 4*count;
+}
---
+++ src/mobility.hpp
@@ -0,0 +1,8 @@
+#ifndef MOBILITY_HPP_INCLUDED
+#define MOBILITY_HPP_INCLUDED
+
+#include "position.hpp"
+
+int piece_mobility(const Position &pos);
+
+#endif
