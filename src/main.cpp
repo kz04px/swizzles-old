a7e6aa2fd1e8927cb5bf9b517245f4a44ec2aa60
kz04px <6536046+kz04px@users.noreply.github.com>
2019-04-04 23:42:57 +0100

    Add some debug information to about

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include "bitboards.hpp"
+#include "hashtable.hpp"
+#include "players/mcts-uct/node.hpp"
+#include "players/mcts-uct/state.hpp"
 #include "protocols.hpp"
 #include "test.hpp"
 #include "zobrist.hpp"
@@ -23,6 +26,15 @@ int main() {
         std::cout << "By kz04px" << std::endl;
         std::cout << "Date: " << __DATE__ << std::endl;
         std::cout << "Time: " << __TIME__ << std::endl;
+        std::cout << std::endl;
+
+        std::cout << "Debug:" << std::endl;
+        std::cout << "sizeof(Move) = " << sizeof(Move) << "B" << std::endl;
+        std::cout << "sizeof(Position) = " << sizeof(Position) << "B"
+                  << std::endl;
+        std::cout << "sizeof(Entry) = " << sizeof(Entry) << "B" << std::endl;
+        std::cout << "sizeof(Node) = " << sizeof(Node) << "B" << std::endl;
+        std::cout << "sizeof(State) = " << sizeof(State) << "B" << std::endl;
 #ifdef NDEBUG
         std::cout << "Release build" << std::endl;
 #else
