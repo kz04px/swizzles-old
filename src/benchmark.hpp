1002b04889eb49d66c31bbd11b1e9de8a3ad9584
kz04px <6536046+kz04px@users.noreply.github.com>
2019-08-03 23:33:13 +0100

    Expand benchmark positions

--- src/benchmark.hpp
+++ src/benchmark.hpp
@@ -4,6 +4,8 @@
 #include <chrono>
 #include <iostream>
 #include <sstream>
+#include <string>
+#include <vector>
 #include "fen.hpp"
 #include "perft/perft.hpp"
 #include "players/all.hpp"
@@ -16,10 +18,18 @@ void benchmark() {
 
     {
         // Perft
+        const std::vector<std::string> fens = {
+            "startpos",
+            "2rq1rk1/pp1bppbp/3p1np1/4n3/3NP3/1BN1BP2/PPPQ2PP/2KR3R w - - 9 12",
+        };
+
         Position pos;
-        set_fen(pos, "startpos");
         const auto t0 = high_resolution_clock::now();
-        perft(pos, 5);
+        for (const auto &fen : fens) {
+            set_fen(pos, fen);
+            perft(pos, 5);
+        }
+
         const auto t1 = high_resolution_clock::now();
         const auto diff = duration_cast<milliseconds>(t1 - t0);
         std::cout << "Perft:  " << diff.count() << "ms" << std::endl;
@@ -28,20 +38,31 @@ void benchmark() {
 
     {
         // Search
+        const std::vector<std::string> fens = {
+            "startpos",
+            "2rq1rk1/pp1bppbp/3p1np1/4n3/3NP3/1BN1BP2/PPPQ2PP/2KR3R w - - 9 12",
+            "r1bqr1k1/1p1n1pbp/p2p2p1/2pP4/Pn2PP2/2N2N2/1P4PP/R1BQRBK1 w - - 2 "
+            "14",
+            "6k1/1b6/2p1p2p/1pPpPpbB/1P3B2/6P1/1q2P2P/5QK1 w - f6 0 30",
+            "2r3k1/5p1p/p3r1p1/4p1QP/2P1P3/8/q4PP1/2R1R1K1 b - - 0 24",
+        };
+
         Hashtable tt(16);
         SearchOptions options;
         options.type = SearchType::Depth;
         options.depth = 9;
         bool stop = false;
         Position pos;
-        set_fen(pos, "startpos");
         std::stringstream buffer;
         std::streambuf *old = std::cout.rdbuf(buffer.rdbuf());
         const auto t0 = high_resolution_clock::now();
-        player::alphabeta(pos, tt, stop, options);
+        for (const auto &fen : fens) {
+            set_fen(pos, fen);
+            player::alphabeta(pos, tt, stop, options);
+        }
         const auto t1 = high_resolution_clock::now();
-        std::cout.rdbuf(old);
         const auto diff = duration_cast<milliseconds>(t1 - t0);
+        std::cout.rdbuf(old);
         std::cout << "Search: " << diff.count() << "ms" << std::endl;
         total += diff.count();
     }
